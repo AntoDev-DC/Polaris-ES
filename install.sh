900fb5c8304ed01debd1be7a62f49dfa900e71ae
Anto <151367564+antolinnin@users.noreply.github.com>
2025-05-17 08:53:20 +0000

    Innecesario

--- install.sh
+++ install.sh
@@ -4,8 +4,10 @@ echo "Gracias por descargar a través del Repositorio de Github de Anto"
 echo "Todos los pasos de instalación en DotCord: https://discord.gg/dotcord"
 npm install
 echo "Preparando archivo .sh para iniciar..."
-mv ./init/start.sh ./start.sh
+mv ./init/init.sh ./init.sh
 rm -- ./init/init.cmd
-rm -rf ./init/¡
+rm -rf ./init
+echo "Borrando Archivos innecesarios..."
+rm -rf ./install.cmd
 echo "Se borrará el archivo de instalación..."
 rm -- "$0"
